fae2e00403e988a58339d582796587f31ba801b2
Bubuka Sharif <sharifbubuka256@gmail.com>
2020-07-25 16:43:04 +0300

    passportjs_login_functionality_1.1
    Innitial commit of functioning basic login/out web app

---
+++ server.js
@@ -0,0 +1,96 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const flash = require('express-flash');
+const session = require('express-session');
+const passport = require('passport');
+const methodoverride = require('method-override');
+const initializePassport = require('./passport-config');
+
+// In a production environment, use Mongodb or Microsoft SQL database here
+const users = [];
+const port = process.env.PORT || 3000;
+
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+);
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodoverride('_method'));
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  next();
+}
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name });
+});
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs', { message: flash.message });
+});
+
+app.post(
+  '/login',
+  checkNotAuthenticated,
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })
+);
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs');
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
+  console.log(users);
+});
+
+app.delete('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/login');
+});
+
+app.listen(port);
